1d814c938c27152cc6f3ec453f002eb1e9a2aef6
Guido <eric5544@gmail.com>
2022-07-23 10:52:16 -0700

    ability to specify custom webviewer folder, simplified imageserver.html

--- README.md
+++ README.md
@@ -53,6 +53,53 @@ let data = await imageserver.generateImage(null,{imageData:myscsdata});
 ```
 
 
+## Using your Own Communicator Server for SCZ Streaming
+The default implementation of the Image Server uses is own server to serve scs models only. However, if you want to generate images from scz models you can run your own Communicator server for image generation. To do this the following steps are required:
+
+1.  Install and run the HOOPS Communicator server on the machine that the image server is running on. From the package this can be accomplished by simply running **start_server.bat** from the quick_start directory.
+2.  Copy imageserver.html into the source webviewer folder ("web_viewer/src" if you use the HOOPS Communicator installation). This file is a slightly modifed version of hoops_web_viewer_sample.html and can be found in the public folder of this package.
+3. Provide the server url and directory to place the scz files when starting the image server (see example below when running the HOOPS Communicator server directly from the installation.)
+
+```
+await imageserver.start({customServer: "http://localhost:11180/imageserver.html?viewer=csr&", sczDirectory:"E:/communicator/HOOPS_Communicator_2022_SP1_U2/quick_start/converted_models/user/sc_models"});
+```
+
+## Using the image server with your own application logic and/or upgrading the version of HOOPS Communicator manually
+While you have a lot of control over the image generation via the callback mechanism, it migth sometimes be necessary to use your own custom logic within the viewer itself. To do this you can provide the folder to the webviewer source during startup.
+
+```
+await imageserver.start({customViewerDirectory:"E:/myviewer"});
+```
+
+The image server expects a file called imageserver.html to be accesible in the top level viewer folder which is a slightly modified version of "hoops_web_viewer_sample.html" with the UI removed and a few callbacks added. See below for the relevant code changes. If you just want to upgrade the version of HOOPS Communicator, all you need to do is copy the imageserver.html file from the public folder of this module into your viewer folder. It will likely work with later versions of HOOPS Communicator. If not simply include the code below to "hoops_web_viewer_sample.html"
+
+```
+
+var currentFrameDrawn = new Date();
+var modelStructureReady = false;
+
+function frameDrawn() {
+    currentFrameDrawn = new Date();
+}
+
+function msready() {
+    modelStructureReady = true;
+}
+
+window.onload = function () {
+    ...
+   //   ui = new Communicator.Ui.Desktop.DesktopUi(hwv, uiConfig);
+
+                  
+        hwv.setCallbacks({
+            frameDrawn: frameDrawn,
+            modelStructureReady: msready,
+        });
+
+```
+
+
+
 ## Communicator Version
 This version of the HC Image Server is using **HOOPS Communicator 2022 SP1 U2**. It will not work with scs files generated with newer versions of HOOPS Communicator.
 
--- app.js
+++ app.js
@@ -8,14 +8,29 @@ const del = require('del');
 var puppeteer;
 var port;
 var browser;
+var customServer = null;
+var sczDirectory = null;
+var customViewerDirectory = null;
 
 
-exports.start = async function (inport) {    
-    if (inport) {
-        port = inport;
+exports.start = async function (params) {    
+    if (params && params.port) {
+        port = params.port;
     }
     else {
         port = 3010;
+    }
+
+    if (params && params.customServer) {
+        customServer = params.customServer;
+        sczDirectory = params.sczDirectory;
+    }
+    else {
+        port = 3010;
+    }
+
+    if (params && params.customViewerDirectory) {
+        customViewerDirectory = params.customViewerDirectory;
     }
 
     const app = express();
@@ -24,12 +39,48 @@ exports.start = async function (inport) {
     app.use(express.urlencoded({ limit: '25mb', extended: false }));
 
 
-    app.use(express.static(path.join(__dirname, 'public')));
+    if (customViewerDirectory) {
+        app.use(express.static(customViewerDirectory));
+    }
+    else {
+        app.use(express.static(path.join(__dirname, 'public')));
+    }
+
     app.listen(port);
 
     puppeteer = require('puppeteer');
     browser = await puppeteer.launch();
 };
+
+
+async function waitUntilFullyDrawn(page, params)
+{
+    return new Promise((resolve, reject) => {
+        let done = false;
+        let modelStructureReadyCalled = false;
+        let interval = setInterval(async () => {
+            const lastframetime = await page.evaluate(() => {
+                return new Date() - currentFrameDrawn;
+            });
+            if (params && params.callback && modelStructureReadyCalled == false) {
+                modelStructureReadyCalled = await page.evaluate(() => {
+                    return modelStructureReady;
+                });
+                if (modelStructureReadyCalled) {
+                    await page.evaluate(params.callback);
+                }
+    
+            }
+            if (lastframetime > 1000) {           
+                if (!done) {
+                    clearInterval(interval);
+                    done = true;
+                    resolve();
+                }    
+            }
+        }, 100);
+    });
+}
 
 
 exports.generateImage = async function (scspath,params) {  
@@ -43,39 +94,83 @@ exports.generateImage = async function (scspath,params) {
     }
     
     var uv4 = uuidv4();
-    fs.writeFileSync(path.join(__dirname,"./public/models/" + uv4 + '.scs'), data);
+    if (sczDirectory) {
+        fs.writeFileSync(sczDirectory + "/" + uv4 + ".scz", data);
+    }
+    else {
+        if (customViewerDirectory) {
+            fs.writeFileSync(customViewerDirectory + "/models/" + uv4, data);
+        }
+        else {        
+            fs.writeFileSync(path.join(__dirname,"./public/models/" + uv4), data);
+        }
+    }
     
-    const page = await browser.newPage();
-    let modelStructureReadyCalled = false;
-    await page.goto('http://localhost:' + port + '/viewer.html?scs=models/' + uv4 + '.scs');
-    let myInterval = setInterval(async () => {
-        const lastframetime = await page.evaluate(() => {
-            return new Date() - currentFrameDrawn;
-        });
-        if (params && params.callback && modelStructureReadyCalled == false) {
-            modelStructureReadyCalled = await page.evaluate(() => {
-                return modelStructureReady;
-            });
-            if (modelStructureReadyCalled) {
-                await page.evaluate(params.callback);
-            }
+    let page = await browser.newPage();
+    if (customServer) {
+        await page.goto(customServer + 'model=' + uv4);
+    }
+    else {
+        await page.goto('http://localhost:' + port + '/imageserver.html?scs=models/' + uv4);
+    }
 
-        }
+    await waitUntilFullyDrawn(page, params);
 
-        if (lastframetime > 1000) {
-            clearInterval(myInterval);
-            await page.screenshot({ path: path.join(__dirname,'./screenshots/' + uv4 + '.png') });
-            let data = fs.readFileSync(path.join(__dirname,'./screenshots/' + uv4 + '.png'));  
-            if (params && params.outputPath) {
-                fs.writeFileSync(params.outputPath, data);
-            }
-            del(path.join(__dirname,"./public/models/" + uv4 + '.scs'),{force: true});
-            del(path.join(__dirname,'./screenshots/' + uv4 + '.png'),{force: true});
-            return data;
+    await page.screenshot({ path: path.join(__dirname, './screenshots/' + uv4 + '.png') });
+    await page.close();
+    let imagedata = fs.readFileSync(path.join(__dirname, './screenshots/' + uv4 + '.png'));
+    if (params && params.outputPath) {
+        fs.writeFileSync(params.outputPath, imagedata);
+    }
+    if (sczDirectory) {
+        del(sczDirectory + "/" + uv4 + ".scz", { force: true });
+    }
+    else {
+        if (customViewerDirectory) {
+            del(customViewerDirectory + "/models/" + uv4, { force: true });
         }
-    }, 100);
+        else {
+            del(path.join(__dirname, "./public/models/" + uv4), { force: true });
+        }
+    }
+    del(path.join(__dirname, './screenshots/' + uv4 + '.png'), { force: true });              
+    return imagedata;
+         
 };
 
 exports.shutdown = async function () {
     await browser.close();
 };
+
+// function myCallback()
+// {
+//     hwv.view.setBackgroundColor(new Communicator.Color(0,0,0));
+// }
+
+
+
+// function myCallback2()
+// {
+//     hwv.view.isolateNodes([446]);
+// }
+
+
+// (async () => {
+//     await this.start({port:3011,customViewerDirectory:"E:/temp/public"});
+//     this.generateImage("E:/communicator/HOOPS_Communicator_2022_SP1_U2/quick_start/converted_models/user/scs_models/DamagedHelmet.scs", {outputPath:"./damagedhelmet.png",callback:myCallback});
+//     this.generateImage("E:/communicator/HOOPS_Communicator_2022_SP1_U2/quick_start/converted_models/user/scs_models/wooden 3d printer (1).scs",{outputPath:"./printer.png",callback:myCallback2});
+// //    this.generateImage("E:/communicator/HOOPS_Communicator_2022_SP1_U2/quick_start/converted_models/user/scs_models/railroadcar.scs");
+// //    this.generateImage("E:/communicator/HOOPS_Communicator_2022_SP1_U2/quick_start/converted_models/user/scs_models/aw_Dives in Misericordia_2D.scs");
+
+// })();
+
+
+// (async () => {
+//     await this.start({customServer: "http://localhost:11180/imageserver.html?viewer=csr&", sczDirectory:"E:/communicator/HOOPS_Communicator_2022_SP1_U2/quick_start/converted_models/user/sc_models"});
+//     await this.generateImage("E:/temp/DamagedHelmet2.scz", {outputPath:"./damagedhelmet.png",callback:myCallback});
+//     await this.generateImage("E:/temp/wooden 3d printer2.scz",{outputPath:"./printer.png",callback:myCallback2});
+// //    this.generateImage("E:/communicator/HOOPS_Communicator_2022_SP1_U2/quick_start/converted_models/user/scs_models/railroadcar.scs");
+// //    this.generateImage("E:/communicator/HOOPS_Communicator_2022_SP1_U2/quick_start/converted_models/user/scs_models/aw_Dives in Misericordia_2D.scs");
+
+// })();
+
